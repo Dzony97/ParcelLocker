251717d592185040cd9f3c72f2f9063189136b22
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-30 14:45:31 +0100

    feat: add data models for Client, ParcelLocker, Package, and Locker entities

---
+++ src/entity.py
@@ -0,0 +1,108 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from abc import ABC, abstractmethod
+from typing import Self, override
+
+
+@dataclass
+class Entity(ABC):
+    id_: int | None = None
+
+    @classmethod
+    @abstractmethod
+    def from_row(cls, *args) -> Self:
+        pass
+
+
+@dataclass
+class Client(Entity):
+    id_: int | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    email: str | None = None
+    phone: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+
+    @classmethod
+    @override
+    def from_row(cls, *args) -> Self:
+        return cls(
+            id_=int(args[0]),
+            first_name=args[1],
+            last_name=args[2],
+            email=args[3],
+            phone=args[4],
+            latitude=float(args[5]),
+            longitude=float(args[6])
+        )
+
+
+@dataclass
+class ParcelLocker(Entity):
+    id_: int | None = None
+    city: str | None = None
+    postal_code: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+    available_slots: int | None = None
+
+    @classmethod
+    @override
+    def from_row(cls, *args) -> Self:
+        return cls(
+            id_=int(args[0]),
+            city=args[1],
+            postal_code=args[2],
+            latitude=float(args[3]),
+            longitude=float(args[4]),
+            available_slots=int(args[5])
+        )
+
+
+@dataclass
+class Package(Entity):
+    id_: int | None = None
+    sender_id: int | None = None
+    receiver_id: int | None = None
+    parcel_locker_id: int | None = None
+    locker_id: int | None = None
+    status: str | None = None
+    size: str | None = None
+    delivered_at: datetime | None = None
+    created_at: datetime = field(default_factory=datetime.now)
+
+    @classmethod
+    @override
+    def from_row(cls, *args) -> Self:
+        return cls(
+            id_=int(args[0]),
+            sender_id=int(args[1]),
+            receiver_id=int(args[2]),
+            parcel_locker_id=int(args[3]),
+            locker_id=int(args[4]),
+            status=args[5],
+            size=args[6],
+            delivered_at=args[7],
+            created_at=args[8]
+        )
+
+
+@dataclass
+class Locker(Entity):
+    id_: int | None = None
+    parcel_locker_id: int | None = None
+    package_id: int | None = None
+    client_id: int | None = None
+    size: str | None = None
+
+    @classmethod
+    @override
+    def from_row(cls, *args) -> Self:
+        return cls(
+            id_=int(args[0]),
+            parcel_locker_id=int(args[1]),
+            package_id=int(args[2]),
+            client_id=int(args[3]),
+            size=args[4]
+        )
