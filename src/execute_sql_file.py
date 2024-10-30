98bcc1b34ed58873169a1cc8339f3dfe39f11c55
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-30 14:29:35 +0100

    feat: add SqlFileExecutor for executing SQL files with managed connections

---
+++ src/execute_sql_file.py
@@ -0,0 +1,20 @@
+from mysql.connector import Error
+from src.database import MySQLConnectionManager, with_db_connection
+
+
+class SqlFileExecutor:
+    def __init__(self, connection_manager: MySQLConnectionManager):
+        self.connection_manager = connection_manager
+
+    @with_db_connection
+    def execute_sql_file(self, sql_file: str):
+        with open (sql_file, 'r') as sql_file:
+            sql_command = sql_file.read()
+
+        try:
+            for command in sql_command.split(';'):
+                command = command.strip()
+                if command:
+                    self._cursor.execute(command)
+        except Error as e:
+            raise e
