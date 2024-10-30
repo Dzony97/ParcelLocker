ac5092e0a8131db3a07654b1f60d06e3115aa4a1
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-30 16:19:33 +0100

    feat: add helper methods for table and column handling in CrudRepository
    Implemented private methods in  to streamline table and column handling for database operations:
    - : derives table name from entity class name, converting it to snake_case.
    - : retrieves column names for SQL insert statements.
    - : formats attribute values of an entity instance for SQL inserts.
    - : formats columns and values for SQL update statements, excluding the  field.

--- src/repository.py
+++ src/repository.py
@@ -2,6 +2,8 @@ from src.entity import Entity, Client, Locker, ParcelLocker, Package
 from src.database import with_db_connection, MySQLConnectionManager
 from typing import Type
 
+import inflection
+
 
 class CrudRepository[T: Entity]:
     def __init__(self, connection_manager: MySQLConnectionManager, entity_type: Type[T]):
@@ -9,3 +11,25 @@ class CrudRepository[T: Entity]:
         self._entity_type = entity_type
 
 
+    def _table_name(self) -> str:
+        return inflection.underscore(self._entity_type.__name__)
+
+    def _column_names_for_insert(self) -> str:
+        return ', '.join([field for field in self._entity_type.__annotations__.keys()])
+
+    def _column_values_for_insert(self, item: T) -> str:
+        fields = [field for field in self._entity_type.__annotations__.keys()]
+        values = [
+            str(getattr(item, field)) if isinstance(getattr(item, field), (int, float))
+            else f"'{getattr(item, field)}'"
+            for field in fields
+        ]
+        return ', '.join(values)
+
+    def _column_names_and_values_for_update(self, item: T) -> str:
+        return ', '.join([
+            f"{field} = {str(getattr(item, field)) if isinstance(getattr(item, field), (int, float))
+            else f"'{getattr(item, field)}'"}"
+            for field in self._entity_type.__annotations__.keys()
+            if field != 'id_'
+        ])
