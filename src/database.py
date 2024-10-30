f1d15997790dcc72d1a9ad4520fa933559755e06
Patrk Palonka <patrykpalonka12@gmail.com>
2024-10-30 14:17:32 +0100

    feat: add  decorator for managed database transactions
    Introduced  decorator to streamline database transactions.
    - Opens a connection and cursor before executing the decorated function.
    - Commits changes on success; rolls back in case of exceptions.
    - Improves error handling and enforces consistent transaction management.
    
    This decorator centralizes connection handling, reducing boilerplate and ensuring transactional integrity.

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from mysql.connector import pooling, MySQLConnection
+from typing import Callable, Any
 import os
 
 load_dotenv()
@@ -21,5 +22,19 @@ class MySQLConnectionManager:
         return self._pool.get_connection()
 
 
+def with_db_connection(func: Callable) -> Callable:
+    def wrapper(self, *args: Any, **kwargs: Any) -> Any:
+        with (self._connection_manager.get_connection() as conn,
+              conn.cursor() as cursor):
+            try:
+                self._conn = conn
+                self._cursor = cursor
+                result = func(self, *args, **kwargs)
+                self._conn.commit()
+                return result
+            except Exception as e:
+                if self._conn:
+                    self._conn.rollback()
+                raise e
 
-
+    return wrapper
