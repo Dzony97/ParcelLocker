671b5397c0ab6cfe5e29b55da4e7e3497133e2bf
Patrk Palonka <patrykpalonka12@gmail.com>
2024-11-13 21:09:24 +0100

    test: add test_has_avilable_slots

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,8 +1,7 @@
 import pytest
 from src.service import ParcelLockerService
 from src.repository import LockerRepository, ClientRepository, PackageRepository, ParcelLockerRepository
-from src.database import MySQLConnectionManager
-
+from src.entity import Size
 
 @pytest.fixture
 def parcel_locker_service(connection_manager) -> ParcelLockerService:
@@ -23,6 +22,11 @@ def parcel_locker_service(connection_manager) -> ParcelLockerService:
 def test_find_client_location(parcel_locker_service):
     client_location = parcel_locker_service.find_client_location(5)
     assert client_location == (52.2297, 21.0122)
+
+
+def test_has_available_slots(parcel_locker_service):
+    excepted_available_slots = parcel_locker_service.has_available_slots(Size.S.value, 1)
+    assert excepted_available_slots == [1, 2]
 
 
 
