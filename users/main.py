0ba302fa64912d24d4999e0df82c6fb863b597b0
Patrk Palonka <patrykpalonka12@gmail.com>
2025-01-17 13:36:16 +0100

    feat: make flask migrations

--- users/main.py
+++ users/main.py
@@ -26,13 +26,13 @@ def create_app() -> Flask:
 
         db_username = getenv('DB_USERNAME', 'user')
         db_password = getenv('DB_PASSWORD', 'user1234')
-        db_port = getenv('DB_PORT', 3306)
+        db_port = getenv('DB_PORT', 3307)
         db_name = getenv('DB_NAME', 'db_1')
         db_hostname = getenv('DB_HOSTNAME', 'mysql')
         app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_hostname}:{db_port}/{db_name}'
         app.config['SQLALCHEMY_ECHO'] = True
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         sa.init_app(app)
-        sa.create_all()
+        migrate = Migrate(app, sa)
 
         return app
