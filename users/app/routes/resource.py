e3ca12dc5607f9846a1f9edd83dc40d8b1fceff3
Patrk Palonka <patrykpalonka12@gmail.com>
2025-01-18 17:16:50 +0100

    add: add resource to user registration

---
+++ users/app/routes/resource.py
@@ -0,0 +1,24 @@
+from flask_restful import Resource, reqparse
+from flask import Response, jsonify, make_response
+from app.db.repository import UserRepository
+from app.db.entity import UserEntity
+from app.service.dto import RegisterUserDto, UserDto
+from app.service.configuration import user_service
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class UserResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help='Username cannot be empty')
+    parser.add_argument('email', type=str, required=True, help='Email cannot be empty')
+    parser.add_argument('password', type=str, required=True, help='Password cannot be empty')
+    parser.add_argument('password_confirmation', type=str, required=True, help='Password confirmation cannot be empty')
+    parser.add_argument('phone_number', type=str, required=True, help='Phone number cannot be empty')
+    parser.add_argument('firstname', type=str, required=True, help='Firstname cannot be empty')
+    parser.add_argument('lastname', type=str, required=True, help='Lastname cannot be empty')
+
+    def post(self) -> Response:
+        register_user_dto = RegisterUserDto.from_dict(UserResource.parser.parse_args())
+        return user_service.register_user(register_user_dto)
