39adcfc6e2a5f79af911f8aeace9555199445053
Patrk Palonka <patrykpalonka12@gmail.com>
2025-01-19 11:56:57 +0100

    add: add mail sender

--- users/main.py
+++ users/main.py
@@ -6,6 +6,7 @@ from os import getenv
 from dotenv import load_dotenv
 from pathlib import Path
 from app.db.configuration import sa
+from app.mail.configuration import MailSender
 from app.db.entity import UserEntity, ClientEntity
 from app.routes.resource import UserResource
 import logging
@@ -30,11 +31,23 @@ def create_app() -> Flask:
         db_port = getenv('DB_PORT', 3307)
         db_name = getenv('DB_NAME', 'db_1')
         db_hostname = getenv('DB_HOSTNAME', 'mysql')
+
         app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_hostname}:{db_port}/{db_name}'
         app.config['SQLALCHEMY_ECHO'] = True
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         sa.init_app(app)
         migrate = Migrate(app, sa)
+
+        mail_settings = {
+            'MAIL_SERVER': getenv('MAIL_SERVER', 'smtp.gmail.com'),
+            'MAIL_PORT': int(getenv('MAIL_PORT', 465)),
+            'MAIL_USE_SSL': bool(getenv('MAIL_USE_SSL', True)),
+            'MAIL_USERNAME': getenv('MAIL_USERNAME', 'testowy2.kmprograms@gmail.com'),
+            'MAIL_PASSWORD': getenv('MAIL_PASSWORD', 'jjtdkjurrfqdgqxu'),
+        }
+        logging.info(mail_settings)
+        app.config.update(mail_settings)
+        MailSender(app, getenv('MAIL_USERNAME', 'testowy2.kmprograms@gmail.com'))
 
         api = Api(app)
         api.add_resource(UserResource, '/users')
